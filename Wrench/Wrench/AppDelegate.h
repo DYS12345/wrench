e6d736b7fd008f3093ab9347013b334c1d886450
dong <609042238@qq.com>
2017-05-22 10:35:42 +0800

    no message

---
+++ Wrench/Wrench/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Wrench
+//
+//  Created by dong on 2017/5/22.
+//  Copyright © 2017年 dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
